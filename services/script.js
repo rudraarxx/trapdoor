836b3a97c8ffe61c81d6e05ae6ed313b70b6f59d
rudraarxx <64283030+rudraarxx@users.noreply.github.com>
2022-08-02 18:38:06 +0530

    Updated

--- services/script.js
+++ services/script.js
@@ -15,3 +15,19 @@ function scrollFunction() {
     document.getElementById("navbar").style.top = "-6.3rem";
   }
 }
+
+var button = document.getElementsByClassName('but'),
+    tabContent = document.getElementsByClassName('tab-content');
+button[0].classList.add('active');
+tabContent[0].style.display = 'block';
+
+
+function city(e, city) {
+    var i;
+    for (i = 0; i < button.length; i++) {
+        tabContent[i].style.display = 'none';
+        button[i].classList.remove('active');
+    }
+    document.getElementById(city).style.display = 'block';
+    e.currentTarget.classList.add('active');
+}
